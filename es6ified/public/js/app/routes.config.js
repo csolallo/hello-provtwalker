8b2af7f0285a456e6e8028b17b4a66890c3a1316
tim walker <tim.twalker@gmail.com>
2015-03-08 20:39:56 -0700

    add text plugin for inlining html as strings

--- es6ified/public/js/app/routes.config.js
+++ es6ified/public/js/app/routes.config.js
@@ -1,14 +1,20 @@
 import 'angular-route'
 
+// import html files as template string using text plugin
+import userTemplate from './user.html!text'
+import searchTemplate from './search.html!text'
+
 var routeConfig = function($routeProvider, $locationProvider){
   $routeProvider
     .when('/search', {
-        templateUrl: 'views/search.html',
+        // templateUrl: 'js/app/search.html',
+        template: searchTemplate,
         controller: 'SearchController',
         controllerAs: 'vm'
     })
     .when('/user/:username', {
-        templateUrl: 'views/user.html',
+        // templateUrl: 'js/app/user.html',
+        template: userTemplate,
         controller: 'UserController'
     })
     .otherwise({redirectTo:'/search'});
--- es6ified/public/js/config.js
+++ es6ified/public/js/config.js
@@ -13,6 +13,7 @@ System.config({
   "map": {
     "angular": "github:angular/bower-angular@1.3.14",
     "angular-route": "github:angular/bower-angular-route@1.3.14",
+    "text": "github:systemjs/plugin-text@0.0.2",
     "github:angular/bower-angular-route@1.3.14": {
       "angular": "github:angular/bower-angular@1.3.14"
     }
